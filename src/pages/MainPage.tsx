2cda31d01f8a711883de018fc47df3237339063d
kkulykk <kkulykk@gmail.com>
2022-03-04 20:55:57 +0200

    Add stylings

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import "./MainPage.css";
 
 const MainPage: React.FC = () => {
   return (
     <div>
-      <Link to={"/world"}>World About Ukraine</Link>
+      <Header />
+      <div className="photo-of-day"></div>
     </div>
   );
 };
