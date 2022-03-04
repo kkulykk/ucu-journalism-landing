3b0190d6b901779c58828e567e6c623f41c1fd49
bmykhaylivvv <bmykhaylivvv@gmail.com>
2022-03-04 20:55:15 +0200

    Add structure folders

---
+++ src/utils/firebaseConfig.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7DrbWVJr4cdgL-x1jJ-XjJRKXj7L0bX4",
+  authDomain: "ucu-journalism-landing.firebaseapp.com",
+  projectId: "ucu-journalism-landing",
+  storageBucket: "ucu-journalism-landing.appspot.com",
+  messagingSenderId: "779410103653",
+  appId: "1:779410103653:web:77a4825ed660956858158c"
+};
+
+const app = initializeApp(firebaseConfig);
