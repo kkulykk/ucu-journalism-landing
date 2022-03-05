1d14dcec5f43e12d1eba3fd5d903a18f01250779
kkulykk <kkulykk@gmail.com>
2022-03-05 22:49:21 +0200

    Add photo to slider

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import {
+  getFirestoreRecords,
+  CollectionNames,
+} from "../services/firebase/firestore";
 
+import { dayPhotosObj } from "../services/models/firestoreDocuments";
 import { ThemeProvider } from "@mui/material";
 import Header from "../components/Header";
 import theme from "../utils/theme";
@@ -13,119 +18,110 @@ import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
 import Footer from "../components/Footer";
 
-{
-  /* <ArticleBox
-          title="Crowds in Prague cheer Ukrainian president's address"
-          date="12 Feb 2022"
-          source="Maria Banias"
-          lead="Cheers erupted in Prague as Ukrainian President Volodymyr Zelenskyy addressed crowds in several European cities remotely via video, seeking support amid the Russian invasion of Ukraine. Cheers erupted in Prague as Ukrainian President Volodymyr Zelenskyy addressed crowds in several European cities remotely via video, seeking support amid the Russian invasion of Ukraine.Cheers erupted in Prague as Ukrainian President Volodymyr Zelenskyy addressed crowds in several European cities remotely via video, seeking support amid the Russian invasion of Ukraine."
-          text="US Secretary of State Antony Blinken has told the BBC that he is convinced Ukraine can win its war with Russia. He could not say how long the conflict would last, but insisted that Ukraine's defeat was not inevitable. Mr Blinken praised the 'extraordinary resilience' of the Ukrainian people. 'If it's the intention of Moscow to try somehow to topple the government and install its own puppet regime, 45 million Ukrainians are going to reject that one way or the other,' he said.
-          
-          The war has already not gone as Russian President Vladimir Putin might have planned, he added.
-          
-          Stiff resistance by Ukrainian forces continued to hamper Russian advances across the country on the ninth day of the invasion.
-          
-          In the south, Russian forces captured areas along the Black Sea coast, and the port city of Mariupol remained surrounded. But the governor of Mykolaiv said Russian troops had been driven out of the city.
-          
-          Ukraine's second city of Kharkiv, in the north, also remained under siege. 
-          
-          Mr Blinken spoke to BBC diplomatic correspondent James Landale after meeting his European Union counterparts in Brussels at the start of a six-day trip to Europe.
-
-He said the international community was committed to doing everything it could to help Ukraine and also to put 'excruciating pressure on Russia to end this war of choice that Vladimir Putin started'.
-
-Asked if he was convinced Ukraine could win, he said: 'Over time, absolutely.'
-
-'I can't tell you how long this will go on. I can't tell you how long it will take. But the idea that Russia can subjugate to its will 45 million people who are ardently fighting for their future and their freedom, that does not involve Russia having its thumb on Ukraine, that tells you a lot.'"
-          imageUrl="https://ichef.bbci.co.uk/news/976/cpsprodpb/6CA2/production/_123401872_hi074101989.jpg"
-        />
-        <ArticleBox
-          title="Pence hits Trump: No room in GOP 'for apologists for Putin"
-          date="11 Feb 2022"
-          source="Maria Banias"
-          lead="Lorem ipsum, dolor sit amet consectetur adipisicing elit. "
-          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati cupiditate dolores maxime magni, corrupti iste. Aliquid nemo quos facilis recusandae magni possimus odio rerum sit assumenda nam, illum ipsa nobis."
-          imageUrl="https://gdb.voanews.com/c42c0000-0aff-0242-eadd-08d9f8888812_w408_r1_s.jpg"
-        /> */
-}
-
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const images = [
-  {
-    source: "Maria Banias",
-    date: "12 Feb 2022",
-    imageUrl:
-      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    source: "Roman Kulyk",
-    date: "5 Mar 2022",
-    imageUrl:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-];
+const MAXX = 7;
+
 const MainPage = () => {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [activeStep, setActiveStep] = useState<number>(0);
+  const [dayPhotosObjects, setDayPhotosObjects] = useState<dayPhotosObj[]>([]);
+
+  const getDayPhotos = async () => {
+    const dayPhotosArray: dayPhotosObj[] = [];
+    try {
+      setIsLoading(true); // switch ON page loader
+      const photos = await getFirestoreRecords(CollectionNames.DAY_PHOTOS, 3);
+
+      photos.forEach((doc) => {
+        const docData = doc.data();
+        const singleDayPhotoObject: dayPhotosObj = new dayPhotosObj(
+          docData.imageUrl,
+          docData.date,
+          docData.source
+        );
+        dayPhotosArray.push(singleDayPhotoObject);
+      });
+
+      setDayPhotosObjects(dayPhotosArray);
+
+      setIsLoading(false);
+      console.log(dayPhotosArray);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const handleStepChange = (step: number) => {
     setActiveStep(step);
+  };
+
+  useEffect(() => {
+    getDayPhotos();
+  }, [3]);
+
+  const Slider = () => {
+    return (
+      <Box>
+        <AutoPlaySwipeableViews
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          // enableMouseEvents
+        >
+          {dayPhotosObjects.map((step, index) => (
+            <div key={step.date}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Box
+                  component="img"
+                  sx={{
+                    height: "60vh",
+                    display: "block",
+                    maxWidth: "100vw",
+                    overflow: "hidden",
+                    width: "100%",
+                    objectFit: "cover",
+                  }}
+                  src={step.imageUrl}
+                  alt={step.source}
+                />
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            backgroundColor: "rgb(0, 0, 0, 0.5)",
+            paddingLeft: 2,
+            fontWeight: "400",
+            color: "white",
+            position: "absolute",
+            top: "60vh",
+            right: 0,
+            paddingRight: "5%",
+          }}
+        >
+          <Typography variant="caption">
+            {dayPhotosObjects[activeStep]?.source}
+          </Typography>
+          <Typography variant="caption">
+            {dayPhotosObjects[activeStep]?.date}
+          </Typography>
+        </Box>
+      </Box>
+    );
   };
 
   return (
     <ThemeProvider theme={theme}>
       <div>
         <Header />
-
-        <Box sx={{ maxWidth: "100vw" }}>
-          <AutoPlaySwipeableViews
-            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-            index={activeStep}
-            onChangeIndex={handleStepChange}
-            // enableMouseEvents
-          >
-            {images.map((step, index) => (
-              <div key={step.date}>
-                {Math.abs(activeStep - index) <= 2 ? (
-                  <Box
-                    component="img"
-                    sx={{
-                      height: "60vh",
-                      display: "block",
-                      maxWidth: "100vw",
-                      overflow: "hidden",
-                      width: "100%",
-                      objectFit: "cover",
-                    }}
-                    src={step.imageUrl}
-                    alt={step.source}
-                  />
-                ) : null}
-              </div>
-            ))}
-          </AutoPlaySwipeableViews>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 2,
-              backgroundColor: "rgb(0, 0, 0, 0.5)",
-              paddingLeft: 2,
-              fontWeight: "400",
-              color: "white",
-              position: "absolute",
-              top: "60vh",
-              right: 0,
-              paddingRight: "5%",
-            }}
-          >
-            <Typography variant="caption">
-              {images[activeStep].source}
-            </Typography>
-            <Typography variant="caption">{images[activeStep].date}</Typography>
-          </Box>
+        <Box sx={{ maxWidth: "100vw", height: "60vh" }}>
+          {isLoading ? "loading" : Slider()}
         </Box>
-
         <Box
           sx={{
             display: "flex",
