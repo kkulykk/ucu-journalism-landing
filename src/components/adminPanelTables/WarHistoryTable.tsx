3d8ac8127f63616539092ca2588549a2801ef0c8
kkulykk <kkulykk@gmail.com>
2022-03-08 20:26:08 +0200

    Add modal fixes

--- src/components/adminPanelTables/WarHistoryTable.tsx
+++ src/components/adminPanelTables/WarHistoryTable.tsx
@@ -7,136 +7,137 @@ import {
 import { WarHistoryObj } from "../../services/models/firestoreDocuments";
 import WarHistoryLeaderInterviewEditModal from "../modals/WarHistoryLeaderInterviewEditModal";
 
-
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 // Constants
 const TABLE_RECORDS_NUMBER = 1;
 
-
-const columns: {id: string, label: string, minWidth: number}[] = [
-  { id: 'title', label: 'Title', minWidth: 200 },
-  { id: 'date', label: 'Date', minWidth: 100 },
-  { id: 'videoUrl', label: 'Video URL', minWidth: 200 },
+const columns: { id: string; label: string; minWidth: number }[] = [
+  { id: "title", label: "Title", minWidth: 200 },
+  { id: "date", label: "Date", minWidth: 100 },
+  { id: "videoUrl", label: "Video URL", minWidth: 200 },
 ];
 
-
-
 const WarHistoryTable = () => {
-  const [warHistoryAdminPanelObject, setWarHistoryAdminPanelObject] = useState<WarHistoryObj[]>([])
+  const [warHistoryAdminPanelObject, setWarHistoryAdminPanelObject] = useState<
+    WarHistoryObj[]
+  >([]);
   const [isTableLoading, setIsTableLoading] = useState<boolean>(false);
-  const [tableRecordsNumber, setTableRecordsNumber] = useState<number>(TABLE_RECORDS_NUMBER)
+  const [tableRecordsNumber, setTableRecordsNumber] =
+    useState<number>(TABLE_RECORDS_NUMBER);
 
-  const [editModalIsOpen, setEditModalIsOpen] = useState<boolean>(false)
+  const [editModalIsOpen, setEditModalIsOpen] = useState<boolean>(false);
 
-  const [editModalTitle, setEditModalTitle] = useState<string>("11111")
-  const [editModalVideoUrl, setEditModalVideoUrll] = useState<string>("33333")
-  const [editModalDate, setEditModalDate] = useState<Date>(new Date())
+  const [editModalTitle, setEditModalTitle] = useState<string>("11111");
+  const [editModalVideoUrl, setEditModalVideoUrll] = useState<string>("33333");
+  const [editModalDate, setEditModalDate] = useState<Date>(new Date());
 
   const getWarHistoryFirestoreRecords = async () => {
     const recordObjectsArray: WarHistoryObj[] = [];
     try {
       setIsTableLoading(true);
-      const records = await getFirestoreRecordsLimit(CollectionNames.WAR_HISTORY, tableRecordsNumber);  
-      
+      const records = await getFirestoreRecordsLimit(
+        CollectionNames.WAR_HISTORY,
+        tableRecordsNumber
+      );
+
       records.forEach((doc) => {
         const docData = doc.data();
-        const singleWarHistoryAdminPanelObject: WarHistoryObj = new WarHistoryObj(
-          doc.id,
-          docData.title,
-          docData.date,
-          docData.videoUrl
-        )
+        const singleWarHistoryAdminPanelObject: WarHistoryObj =
+          new WarHistoryObj(
+            doc.id,
+            docData.title,
+            docData.date,
+            docData.videoUrl
+          );
 
         recordObjectsArray.push(singleWarHistoryAdminPanelObject);
-      })
+      });
 
       setWarHistoryAdminPanelObject(recordObjectsArray);
       setIsTableLoading(false);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   useEffect(() => {
-    getWarHistoryFirestoreRecords()
-  }, [tableRecordsNumber])
+    getWarHistoryFirestoreRecords();
+  }, [tableRecordsNumber]);
 
   const openEditModal = (title: string, videoUrl: string, date: Date): void => {
     setEditModalTitle(title);
     setEditModalVideoUrll(videoUrl);
     setEditModalDate(date);
 
-    setEditModalIsOpen(true);    
-  }
+    setEditModalIsOpen(true);
+  };
 
   const renderRows = () => {
     return warHistoryAdminPanelObject.map((rowObject) => {
       return (
-        <Box>
-
-          <TableRow onClick={() => openEditModal(rowObject.title, rowObject.videoUrl, rowObject.dateObj)}>
-            {
-              columns.map((column) => {
-                const value = (rowObject as any)[column.id]; // Yeah, that pretty interesting fix from StackOverflow
-                return (
-                  <TableCell key={column.id}>
-                    {value}
-                  </TableCell>
-                )
-              })
-            }
-          </TableRow>
-            </Box>
-      )
-    })
-  }
-
-
-
-
+        <TableRow
+          sx={{ cursor: "pointer" }}
+          onClick={() =>
+            openEditModal(
+              rowObject.title,
+              rowObject.videoUrl,
+              rowObject.dateObj
+            )
+          }
+        >
+          {columns.map((column) => {
+            const value = (rowObject as any)[column.id]; // Yeah, that pretty interesting fix from StackOverflow
+            return <TableCell key={column.id}>{value}</TableCell>;
+          })}
+        </TableRow>
+      );
+    });
+  };
 
   return (
-    <Box>
-      <WarHistoryLeaderInterviewEditModal modalHeading={"War History Post | Id: " + "ID"} title={editModalTitle} videoUrl={editModalVideoUrl} date={editModalDate} modalIsOpen={editModalIsOpen} setModalIsOpen={setEditModalIsOpen} />
-
-      <TableContainer sx={{ height: 250, background: "aqua" }}>
-        <Table>
-          <TableHead>
-            <TableRow>
+    <TableContainer sx={{ height: 250, background: "aqua" }}>
+      <Table>
+        <TableHead>
+          <TableRow>
             {columns.map((column) => (
-                  <TableCell
-                  key={column.id}
-                    style={{ top: 57, minWidth: column.minWidth }}
-                    >
-                    {column.label}
-                  </TableCell>
-                ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {isTableLoading ? "Loading" : renderRows()}
-            <Button
-              disabled={isTableLoading}
-              variant="outlined"
-              sx={{ marginTop: 5 }}
-              onClick={() => setTableRecordsNumber(tableRecordsNumber + TABLE_RECORDS_NUMBER)}
-              >
-              Load more
-            </Button>
-          </TableBody>
-
-        </Table>
-      </TableContainer>
-    </Box>
+              <TableCell key={column.id} sx={{ top: 57 }}>
+                {column.label}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {isTableLoading ? "Loading" : renderRows()}
+          <Button
+            disabled={isTableLoading}
+            variant="outlined"
+            sx={{ marginTop: 5 }}
+            onClick={() =>
+              setTableRecordsNumber(tableRecordsNumber + TABLE_RECORDS_NUMBER)
+            }
+          >
+            Load more
+          </Button>
+        </TableBody>
+      </Table>
+      <WarHistoryLeaderInterviewEditModal
+        modalHeading={"War History Post | Id: " + "ID"}
+        title={editModalTitle}
+        videoUrl={editModalVideoUrl}
+        date={editModalDate}
+        modalIsOpen={editModalIsOpen}
+        setModalIsOpen={setEditModalIsOpen}
+      />
+    </TableContainer>
   );
 };
 
--- src/components/modals/WarHistoryLeaderInterviewEditModal.tsx
+++ src/components/modals/WarHistoryLeaderInterviewEditModal.tsx
@@ -22,8 +22,8 @@ interface Props {
 }
 
 const WarHistoryLeaderInterviewEditModal = (props: Props) => {
-  const [title, setTitle] = useState<string>(props.title)
-  const [videoUrl, setVideoUrl] = useState<string>(props.videoUrl)
+  const [title, setTitle] = useState<string>(props.title);
+  const [videoUrl, setVideoUrl] = useState<string>(props.videoUrl);
   const [dateSelected, setDateSelected] = useState<Date | null>(props.date);
 
   const style = {
@@ -50,7 +50,7 @@ const WarHistoryLeaderInterviewEditModal = (props: Props) => {
         <Box sx={style}>
           <Box sx={{ p: 5, overflow: "scroll" }}>
             <Typography variant="h3" sx={{ marginBottom: 1 }}>
-              Edit {props.modalHeading} 
+              Edit {props.modalHeading}
             </Typography>
             <Box
               sx={{
@@ -62,17 +62,19 @@ const WarHistoryLeaderInterviewEditModal = (props: Props) => {
             >
               <Box sx={{ display: "flex", gap: 2 }}>
                 <Typography variant="h3" sx={{ marginBottom: 1 }}>
-                  {props.title} 
+                  {props.title}
                 </Typography>
                 <TextField
                   sx={{ width: "100%" }}
                   label="Title"
                   variant="outlined"
-                  value={title}
+                  // value={title}
+                  defaultValue={props.title}
+                  onChange={(event) => console.log(event.target.value)}
                 />
-                <Typography variant="h3" sx={{ marginBottom: 1 }}>
-                  {props.date.toISOString()} 
-                </Typography>
+                {/* <Typography variant="h3" sx={{ marginBottom: 1 }}>
+                  {props.date.toISOString()}
+                </Typography> */}
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
                   <DatePicker
                     label="Date"
@@ -81,14 +83,17 @@ const WarHistoryLeaderInterviewEditModal = (props: Props) => {
                       setDateSelected(newValue);
                     }}
                     renderInput={(params) => <TextField {...params} />}
-
                   />
                 </LocalizationProvider>
               </Box>
               <Typography variant="h3" sx={{ marginBottom: 1 }}>
-                  {props.videoUrl} 
-                </Typography>
-              <TextField label="Video URL" variant="outlined" value={videoUrl}/>
+                {props.videoUrl}
+              </Typography>
+              <TextField
+                label="Video URL"
+                variant="outlined"
+                value={videoUrl}
+              />
             </Box>
             <Button
               color="primary"
@@ -104,7 +109,7 @@ const WarHistoryLeaderInterviewEditModal = (props: Props) => {
               sx={{ marginLeft: "2%" }}
               onClick={() => {
                 props.setModalIsOpen(false);
-                console.log("CLOSE")
+                console.log("CLOSE");
               }}
             >
               Close
