13469e1236b389856a4a92cb8fe3a625514a5314
bmykhaylivvv <bmykhaylivvv@gmail.com>
2022-03-11 19:55:54 +0200

    Add .env file

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7DrbWVJr4cdgL-x1jJ-XjJRKXj7L0bX4",
-  authDomain: "ucu-journalism-landing.firebaseapp.com",
-  projectId: "ucu-journalism-landing",
-  storageBucket: "ucu-journalism-landing.appspot.com",
-  messagingSenderId: "779410103653",
-  appId: "1:779410103653:web:77a4825ed660956858158c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
